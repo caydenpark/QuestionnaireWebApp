a30b1904978eb7a94f594247a033cd46f06b939a
conhop30 <conhop30@gmail.com>
2023-10-25 15:25:20 -0600

    Question displaying with minefield of comments.

--- questionnaire/src/index.js
+++ questionnaire/src/index.js
@@ -8,62 +8,94 @@ window.cSharp = 0;
 window.javaScript = 0;
 window.sql = 0;
 window.html = 0;
+window.scores = [0, 0, 0, 0, 0] // Five for five languages.
+                                // Should be made dynamic in the future.
 
+// Give index.html something to communicate with.
 const container = document.getElementById('questionnaire');
+// Make it possible to display to index.html.
+const root = ReactDOM.createRoot(container);
 
-// Questions
+// Question functions
+//const questions = [
 function question1() {
   return (
     <div>
       <h1>Question 1</h1>
       <h3>What is your experience level with programming?</h3>
-      <input type="radio" id="noXP" name="question1" value="noXP" onChange={handleRadioChange} />
-      <label htmlFor="noXP">None</label><br />
-      <input type="radio" id="someXP" name="question1" value="someXP" onChange={handleRadioChange} />
-      <label htmlFor="someXP">Some</label><br />
-      <input type="radio" id="hobbyXP" name="question1" value="hobbyXP" onChange={handleRadioChange} />
-      <label htmlFor="hobbyXP">Hobbyist</label><br />
-      <input type="radio" id="engineerXP" name="question1" value="engineerXP" onChange={handleRadioChange} />
+      <input type="radio" id="noXP" name="question1" value="noXP" onChange={onAnswerReceived} />
+      <label htmlFor="noXP">None</label>
+      <br />
+      <input type="radio" id="someXP" name="question1" value="someXP" onChange={onAnswerReceived} />
+      <label htmlFor="someXP">Some</label>
+      <br />
+      <input type="radio" id="hobbyXP" name="question1" value="hobbyXP" onChange={onAnswerReceived} />
+      <label htmlFor="hobbyXP">Hobbyist</label>
+      <br />
+      <input type="radio" id="engineerXP" name="question1" value="engineerXP" onChange={onAnswerReceived} />
       <label htmlFor="engineerXP">Engineer</label>
     </div>
   );
-}
+} //,
 
 function question2() {
   return (
     <div>
       <h1>Question?</h1>
       <h3>Is this another question?</h3>
-      <input type="radio" id="noXP" name="question2" value="noXP" onChange={handleRadioChange} />
-      <label htmlFor="noXP">Yes</label>
+      <input type="radio" id="yes" name="question2" value="yes" onChange={onAnswerReceived} />
+      <label htmlFor="yes">Yes</label>
       <br />
-      <input type="radio" id="someXP" name="question2" value="someXP" onChange={handleRadioChange} />
-      <label htmlFor="someXP">No</label>
+      <input type="radio" id="no" name="question2" value="no" onChange={onAnswerReceived} />
+      <label htmlFor="no">No</label>
       <br />
-      <input type="radio" id="hobbyXP" name="question2" value="hobbyXP" onChange={handleRadioChange} />
-      <label htmlFor="hobbyXP">I thought so, but...</label>
+      <input type="radio" id="confused" name="question2" value="confused" onChange={onAnswerReceived} />
+      <label htmlFor="confused">I thought so, but...</label>
       <br />
-      <input type="radio" id="engineerXP" name="question2" value="engineerXP" onChange={handleRadioChange} />
-      <label htmlFor="engineerXP">You tell me</label>
+      <input type="radio" id="challenge" name="question2" value="challenge" onChange={onAnswerReceived} />
+      <label htmlFor="challenge">You tell me</label>
     </div>
   );
 }
+// ]
 
-// Apply weight values to given language variables.
-function handleRadioChange() {
-
-}
-
-// Part of a WHILE loop that exits on the questionnaire being completed
-// Display the current question
-// const i_question = 1; // Set the current question index
-// let notAnswered = True;
-const root = ReactDOM.createRoot(container);
-
-// // Drives the questions.
-// while(notAnswered) {
-//   root.render(question + i_question + ())
+// // Tracks the current question.
+// let answered = true;
+// for (let i_tracker = 0; i_tracker < questions.length; i_tracker++) {
+//   // Displays the current question.
+//   while(!answered) {
+//     root.render(questions[i_tracker]());
+//    }
+//    // Move on to the next question.
+//    answered = false;
 // }
+
+// Apply weighted values to given language variables.
+function onAnswerReceived(event, i_tracker) {
+  // let selectedValue = 0;
+  // // Question 1 calculations...
+  // if (questions[i_tracker] === 1) {
+  //   // Option 1...
+  //   if (selectedValue === 0) {
+  //     window.python = 4;
+  //     window.cSharp = 3;
+  //     window.javaScript = 3;
+  //     window.sql = 4;
+  //     window.html = 4;
+  //   }
+  //   // Option 2... and so on.
+  //   else if (selectedValue === 1) {
+  //   }
+  //   else if (selectedValue === 2) {
+  //   }
+  //   else if (selectedValue === 3) {
+  //   }
+  //   else {
+  //     // Must be option 5.
+  //   }
+  // // Question 2 calculations... and so on.
+  // }
+}
 
 root.render(question1());
 root.render(question2());
